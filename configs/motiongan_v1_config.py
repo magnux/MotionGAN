a7d58b9e5744bf66d46a3319cc52c136f1effc04
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2018-02-19 16:36:46 +0100

    added a few more configs

--- configs/motiongan_v1_config.py
+++ configs/motiongan_v1_config.py
@@ -1,1 +1,4 @@
-{}
+{
+    # Model version to train
+    'model_version': 'v1',
+}
---
+++ configs/motiongan_v1_msrc_config.py
@@ -0,0 +1,7 @@
+{
+    # Datasets: MSRC12, NTURGBD
+    'data_set': 'MSRC12',
+    'data_set_version': '',
+    # Model version to train
+    'model_version': 'v1',
+}

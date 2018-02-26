dfba45565a034d37fa25f6cafde8306a8553830b
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2018-02-26 20:53:47 +0100

    disabled vae rec loss

--- models/motiongan.py
+++ models/motiongan.py
@@ -243,7 +243,7 @@ class _MotionGAN(object):
         gen_losses['gen_loss_rec'] = self.rec_scale * loss_rec
 
         if self.vae_pose_enc:
-            gen_losses['vae_loss_rec'] = self.vae_scale * K.mean(self.vae_enc_head - self.vae_gen_head)
+            # gen_losses['vae_loss_rec'] = self.vae_scale * K.mean(self.vae_enc_head - self.vae_gen_head)
             gen_losses['vae_loss_kl'] = self.vae_scale * K.mean(- 0.5 * K.sum(1 + self.vae_z_log_var -
                                                                 K.square(self.vae_z_mean) -
                                                                 K.exp(self.vae_z_log_var), axis=-1))

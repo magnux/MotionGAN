8226a1b4fc6940ffd877010cba38d60ef8f1fb6e
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2018-03-07 11:17:38 +0100

    added config

---
+++ configs/motiongan_v2_vae_shift_config.py
@@ -0,0 +1,9 @@
+{
+    # Model version to train
+    'model_version': 'v2',
+    # Use pose VAE
+    'use_pose_vae': True,
+    'use_shifting': True,
+    # Path to pretrained pose VAE
+    'pose_vae_save_path': 'save/pose_vae_v1',
+}

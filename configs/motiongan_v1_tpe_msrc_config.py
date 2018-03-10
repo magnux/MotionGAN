ad3336826a3fac596a66cfb4163fa9578fee8da9
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2018-03-10 15:38:02 +0100

    changed old training params for MSRC dataset

--- configs/motiongan_v1_tpe_msrc_config.py
+++ configs/motiongan_v1_tpe_msrc_config.py
@@ -4,6 +4,10 @@
     'data_set_version': '',
     # Model version to train
     'model_version': 'v1',
+    # Final epoch
+    'num_epochs': 200,
+    # Multiplies length of epoch, useful for tiny datasets
+    'epoch_factor': 10,
     # Time preserving embedding (NOT COMPATIBLE with latent factor model)
     'time_pres_emb': True,
 }
--- configs/motiongan_v2_msrc_config.py
+++ configs/motiongan_v2_msrc_config.py
@@ -4,5 +4,8 @@
     'data_set_version': '',
     # Model version to train
     'model_version': 'v2',
-    'num_epochs': 500,
+    # Final epoch
+    'num_epochs': 200,
+    # Multiplies length of epoch, useful for tiny datasets
+    'epoch_factor': 10,
 }

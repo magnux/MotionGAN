5152154451e61c5cd926cf583e1f3617f4659d6a
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2018-05-05 17:01:07 +0200

    improved processing scripts for H36 dataset

--- utils/human36_expmaps_to_h5.py
+++ utils/human36_expmaps_to_h5.py
@@ -8,67 +8,66 @@ import re
 from glob import glob
 from tqdm import tqdm
 
-found_files = [file for file in glob('extracted_expmaps/S*/*.txt')]
-
-print('Processing {} files...'.format(len(found_files)))
-
+dataset = 'Human36_expmaps'
 subjects = ['S1', 'S5', 'S6', 'S7', 'S8', 'S9', 'S11']
 cameras = ['54138969', '55011271', '58860488', '60457274']
 actions = ['directions', 'discussion', 'eating', 'greeting', 'phoning',
            'posing', 'purchases', 'sitting', 'sittingdown', 'smoking',
            'takingphoto', 'waiting', 'walking', 'walkingdog', 'walkingtogether']
 
-prog = re.compile('(S\d+)/([^_]+)_(\d).txt')
-
 
 def read_pose(x):
     return np.reshape(np.transpose(np.array(x), [1, 0]), [33, 3, -1])
 
 
-dataset = 'Human36_expmaps'
-h5file = h5.File(dataset + "v1.h5", "w")
-max_len = 0
-for f, found_file in enumerate(tqdm(found_files)):
-    confpars = prog.findall(found_file)[0]
-    subject = [i for i, x in enumerate(subjects) if x == confpars[0]][0]
-    action = [i for i, x in enumerate(actions) if x in confpars[1]][0]
-    subaction = int(confpars[2])
+if __name__ == "__main__":
+    found_files = [file for file in glob('extracted_expmaps/S*/*.txt')]
+    print('Processing {} files...'.format(len(found_files)))
 
-    # print(found_file)
-    # print(subject, action, subaction)
+    prog = re.compile('(S\d+)/([^_]+)_(\d).txt')
+    h5file = h5.File(dataset + "v1.h5", "w")
+    max_len = 0
+    for f, found_file in enumerate(tqdm(found_files)):
+        confpars = prog.findall(found_file)[0]
+        subject = [i for i, x in enumerate(subjects) if x == confpars[0]][0]
+        action = [i for i, x in enumerate(actions) if x in confpars[1]][0]
+        subaction = int(confpars[2])
 
-    subarray = np.array(subject + 1)
-    actarray = np.array(action + 1)
+        # print(found_file)
+        # print(subject, action, subaction)
 
-    posearray = []
-    lines = open(found_file).readlines()
-    for line in lines:
-        line = line.strip().split(',')
-        if len(line) > 0:
-            posearray.append(np.array([np.float32(x) for x in line]))
+        subarray = np.array(subject + 1)
+        actarray = np.array(action + 1)
 
-    posearray = read_pose(posearray)
+        posearray = []
+        lines = open(found_file).readlines()
+        for line in lines:
+            line = line.strip().split(',')
+            if len(line) > 0:
+                posearray.append(np.array([np.float32(x) for x in line]))
 
-    # S5 will be the Validate split the rest of the subjects are the training set
-    datasplit = 'Validate' if subjects[subject] == 'S5' else 'Train'
+        posearray = read_pose(posearray)
 
-    datapath = '{}/{}/SEQ{}/'.format(dataset, datasplit, f)
-    h5file.create_dataset(
-        datapath + 'Subject', np.shape(subarray),
-        dtype='int32', data=subarray
-    )
-    h5file.create_dataset(
-        datapath + 'Action', np.shape(actarray),
-        dtype='int32', data=actarray
-    )
-    h5file.create_dataset(
-        datapath + 'Pose', np.shape(posearray),
-        dtype='float32', data=posearray
-    )
-    max_len = max(max_len, posearray.shape[2])
+        # S5 will be the Validate split the rest of the subjects are the training set
+        datasplit = 'Validate' if subjects[subject] == 'S5' else 'Train'
 
-print('Dataset sample: ')
-print(h5file.get(dataset + '/Validate/'))
-print('max length', max_len)
-h5file.flush()
-h5file.close()
+        datapath = '{}/{}/SEQ{}/'.format(dataset, datasplit, f)
+        h5file.create_dataset(
+            datapath + 'Subject', np.shape(subarray),
+            dtype='int32', data=subarray
+        )
+        h5file.create_dataset(
+            datapath + 'Action', np.shape(actarray),
+            dtype='int32', data=actarray
+        )
+        h5file.create_dataset(
+            datapath + 'Pose', np.shape(posearray),
+            dtype='float32', data=posearray
+        )
+        max_len = max(max_len, posearray.shape[2])
+
+    print('Dataset sample: ')
+    print(h5file.get(dataset + '/Validate/'))
+    print('max length', max_len)
+    h5file.flush()
+    h5file.close()
--- utils/human36_skels_to_h5.py
+++ utils/human36_skels_to_h5.py
@@ -10,62 +10,61 @@ import re
 from glob import glob
 from tqdm import tqdm
 
-found_files = [file for file in glob('extracted/S*/MyPoseFeatures/D3_Positions/*.cdf')]
-
-print('Processing {} files...'.format(len(found_files)))
-
+dataset = 'Human36'
 subjects = ['S1', 'S5', 'S6', 'S7', 'S8', 'S9', 'S11']
 cameras = ['54138969', '55011271', '58860488', '60457274']
 actions = ['Directions', 'Discussion', 'Eating', 'Greeting', 'Phoning',
            'Posing', 'Purchases', 'Sitting', 'SittingDown', 'Smoking',
            'Photo', 'Waiting', 'Walking', 'WalkDog', 'WalkTogether']
 
-prog = re.compile('(S\d+)/MyPoseFeatures/D3_Positions/([^.]+).cdf')
-
 
 def read_pose(x):
     return np.reshape(np.transpose(np.array(x), [2, 0, 1]), [32, 3, -1])
 
 
-dataset = 'Human36'
-h5file = h5.File(dataset + "v1.h5", "w")
-max_len = 0
-for f, found_file in enumerate(tqdm(found_files)):
-    confpars = prog.findall(found_file)[0]
-    subject = [i for i, x in enumerate(subjects) if x == confpars[0]][0]
-    action = [i for i, x in enumerate(actions) if x in confpars[1]][0]
+if __name__ == "__main__":
+    found_files = [file for file in glob('extracted/S*/MyPoseFeatures/D3_Positions/*.cdf')]
+    print('Processing {} files...'.format(len(found_files)))
 
-    # print(found_file)
-    # print(subject, action)
+    prog = re.compile('(S\d+)/MyPoseFeatures/D3_Positions/([^.]+).cdf')
+    h5file = h5.File(dataset + "v1.h5", "w")
+    max_len = 0
+    for f, found_file in enumerate(tqdm(found_files)):
+        confpars = prog.findall(found_file)[0]
+        subject = [i for i, x in enumerate(subjects) if x == confpars[0]][0]
+        action = [i for i, x in enumerate(actions) if x in confpars[1]][0]
 
-    subarray = np.array(subject + 1)
-    actarray = np.array(action + 1)
+        # print(found_file)
+        # print(subject, action)
 
-    pose3dcdf = pycdf.CDF(found_file)
+        subarray = np.array(subject + 1)
+        actarray = np.array(action + 1)
 
-    posearray = read_pose(pose3dcdf['Pose'])
-    pose3dcdf.close()
+        pose3dcdf = pycdf.CDF(found_file)
 
-    # S5 will be the Validate split the rest of the subjects are the training set
-    datasplit = 'Validate' if subjects[subject] == 'S5' else 'Train'
+        posearray = read_pose(pose3dcdf['Pose'])
+        pose3dcdf.close()
 
-    datapath = '{}/{}/SEQ{}/'.format(dataset, datasplit, f)
-    h5file.create_dataset(
-        datapath + 'Subject', np.shape(subarray),
-        dtype='int32', data=subarray
-    )
-    h5file.create_dataset(
-        datapath + 'Action', np.shape(actarray),
-        dtype='int32', data=actarray
-    )
-    h5file.create_dataset(
-        datapath + 'Pose', np.shape(posearray),
-        dtype='float32', data=posearray
-    )
-    max_len = max(max_len, posearray.shape[2])
+        # S5 will be the Validate split the rest of the subjects are the training set
+        datasplit = 'Validate' if subjects[subject] == 'S5' else 'Train'
 
-print('Dataset sample: ')
-print(h5file.get(dataset + '/Validate/'))
-print('max length', max_len)
-h5file.flush()
-h5file.close()
+        datapath = '{}/{}/SEQ{}/'.format(dataset, datasplit, f)
+        h5file.create_dataset(
+            datapath + 'Subject', np.shape(subarray),
+            dtype='int32', data=subarray
+        )
+        h5file.create_dataset(
+            datapath + 'Action', np.shape(actarray),
+            dtype='int32', data=actarray
+        )
+        h5file.create_dataset(
+            datapath + 'Pose', np.shape(posearray),
+            dtype='float32', data=posearray
+        )
+        max_len = max(max_len, posearray.shape[2])
+
+    print('Dataset sample: ')
+    print(h5file.get(dataset + '/Validate/'))
+    print('max length', max_len)
+    h5file.flush()
+    h5file.close()

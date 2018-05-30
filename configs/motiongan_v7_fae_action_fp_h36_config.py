6983afe03ed3402155086d6fcad03e8b9f0287dc
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2018-05-30 16:36:41 +0200

    changed batch_size

--- configs/motiongan_v7_fae_action_fp_h36_config.py
+++ configs/motiongan_v7_fae_action_fp_h36_config.py
@@ -23,7 +23,7 @@
     # How fast should we learn?
     'learning_rate': 1.0e-3,
     # It's the batch size
-    'batch_size': 256,
+    'batch_size': 128,
     # Multiplies length of epoch, useful for tiny datasets
     'epoch_factor': 200,
     # Number of the random picks (0 == deactivated)

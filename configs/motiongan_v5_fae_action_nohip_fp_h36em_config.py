6f3f7727b8d225273198ed2d5c735be12a7533b2
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2018-06-12 14:41:44 +0200

    changed training params

--- configs/motiongan_v5_fae_action_nohip_fp_h36em_config.py
+++ configs/motiongan_v5_fae_action_nohip_fp_h36em_config.py
@@ -19,9 +19,9 @@
     'coherence_loss': False,
 
     # How fast should we learn?
-    'learning_rate': 5e-6,
+    'learning_rate': 1e-5,
     # It's the batch size
-    'batch_size': 64,
+    'batch_size': 128,
     # Multiplies length of epoch, useful for tiny datasets
     'epoch_factor': 256,
     # Number of the random picks (0 == deactivated)

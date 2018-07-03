a6836e5e7977601086ce28fccdb3a1bdcd3a148c
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2018-07-03 10:36:00 +0200

    changed training params

--- configs/motiongan_v9_fae_action_fp_h36_config.py
+++ configs/motiongan_v9_fae_action_fp_h36_config.py
@@ -19,13 +19,11 @@
     'action_cond': True,
     # Augment data on training
     'augment_data': True,
-    # Coherence on generated sequences loss
-    # 'coherence_loss': True,
 
     # How fast should we learn?
     'learning_rate': 1e-5,
     # It's the batch size
-    'batch_size': 128,
+    'batch_size': 64,
     # Multiplies length of epoch, useful for tiny datasets
     'epoch_factor': 256,
     # Number of the random picks (0 == deactivated)
